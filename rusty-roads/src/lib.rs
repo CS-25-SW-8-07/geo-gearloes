046919cff82933b1dc7ca76dc4fb2c4e11ea5c36
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2025-02-25 13:02:01 +0100

    Converted to struct of vectors

--- rusty-roads/src/lib.rs
+++ rusty-roads/src/lib.rs
@@ -9,33 +9,33 @@ pub enum Direction {
 type Id = usize;
 
 pub struct Road<T: CoordNum> {
-    pub geom: LineString<T>,
-    pub id: Id,
-    pub osm_id: u64,
-    pub code: u16,
-    pub direction: Direction,
-    pub maxspeed: u16,
-    pub layer: i16,
-    pub bridge: bool,
-    pub tunnel: bool,
+    pub geom: Vec<LineString<T>>,
+    pub id: Vec<Id>,
+    pub osm_id: Vec<u64>,
+    pub code: Vec<u16>,
+    pub direction: Vec<Direction>,
+    pub maxspeed: Vec<u16>,
+    pub layer: Vec<i16>,
+    pub bridge: Vec<bool>,
+    pub tunnel: Vec<bool>,
 }
 
 pub struct Name {
-    pub id: Id,
-    pub name: String,
+    pub id: Vec<Id>,
+    pub name: Vec<String>,
 }
 
 pub struct RefMany {
-    pub road_id: Id,
-    pub ref_id: Id,
+    pub road_id: Vec<Id>,
+    pub ref_id: Vec<Id>,
 }
 
 pub struct Ref {
-    pub id: Id,
-    pub reff: String,
+    pub id: Vec<Id>,
+    pub reff: Vec<String>,
 }
 
 pub struct FeatureClass {
-    pub code: u16,
-    pub fclass: String,
+    pub code: Vec<u16>,
+    pub fclass: Vec<String>,
 }
