d9b05434d9fe7b03d4357d10aa084a9ade0a1177
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-02-25 13:52:30 +0100

    added connection function + test

--- atlas/src/lib.rs
+++ atlas/src/lib.rs
@@ -1,4 +1,11 @@
 use rusty_roads::*;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+
+pub async fn bind(conn: &str, max_conn: Option<u32>) -> Result<Pool<Postgres>, sqlx::Error> {
+    PgPoolOptions::new()
+        .max_connections(max_conn.unwrap_or(1))
+        .connect_lazy(conn)
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
@@ -6,7 +13,32 @@ pub fn add(left: usize, right: usize) -> usize {
 
 #[cfg(test)]
 mod tests {
+
+    use dotenvy::dotenv;
+
     use super::*;
+    use std::env;
+
+    // const USERNAME: String = dotenv!("USERNAME");
+    // const PASSWORD: String = dotenv!("PASSWORD");
+    // const ADDRESS: String = dotenv!("ADDRESS");
+    // const DATABASE: String = dotenv!("DATABASE");
+    #[async_std::test]
+    async fn it_connects() {
+        //TODO: ideally, reading environment variables should be done at compile time
+        dotenv().expect("Failed to read environment variables");
+        let username = env::var("USERNAME").expect("`USERNAME` environment variable should be set");
+        let password = env::var("PASSWORD").expect("`PASSWORD` environment variable should be set ");
+        let address = env::var("ADDRESS").expect("`ADDRESS` environment variable should be set ");
+        let database = env::var("DBNAME").expect("`DBNAME` environment variable should be set ");
+
+        let pool = bind(
+            &format!("postgres://{}:{}@{}/{}",&username,&password,&address,&database),
+            Some(1),
+        )
+        .await;
+        assert!(matches!(pool, Ok(x) if x.options().get_max_connections() >= 1))
+    }
 
     #[test]
     fn it_works() {
