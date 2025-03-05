97b76f67f32dd589f7564c008549829768b17624
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2025-03-05 11:37:56 +0100

    WIP

--- location_obfuscation/src/lib.rs
+++ location_obfuscation/src/lib.rs
@@ -6,19 +6,3 @@ use rstar::{PointDistance, RTree, RTreeObject};
 //use rusty_roads::Road;
 
 
-
-
-pub struct RoadIndex<T: RTreeObject + PointDistance + Clone> {
-    pub index: RTree<GeomWithData<T, u64>>,
-}
-
-impl<T: RTreeObject + PointDistance + Clone> RoadIndex<T> {
-    pub fn new(ids: &[u64], roads: &[T]) -> RoadIndex<T> {
-        let geomdata: Vec<GeomWithData<T, u64>> = roads.iter().zip(ids.iter()).map(|(road, id)| GeomWithData::<T, u64>::new(road.clone(), *id)).collect();
-
-        RoadIndex::<T> {
-            index: RTree::<GeomWithData<T, u64>>::bulk_load(geomdata),
-        }
-
-    }
-}
--- rusty-roads/src/lib.rs
+++ rusty-roads/src/lib.rs
@@ -1,4 +1,6 @@
 use geo_types::{CoordNum, LineString};
+use rstar::primitives::GeomWithData;
+use rstar::{PointDistance, RTree, RTreeObject};
 
 pub enum Direction {
     Forward,
@@ -23,6 +25,8 @@ pub struct RoadRow<T: CoordNum> {
 }
 
 pub struct Road<T: CoordNum> {
+    pub rtree: RTree<GeomWithData<T, u64>>,
+
     pub id: Vec<Id>, // Primary key
     pub geom: Vec<LineString<T>>,
     pub osm_id: Vec<u64>,
@@ -32,6 +36,43 @@ pub struct Road<T: CoordNum> {
     pub layer: Vec<i16>,
     pub bridge: Vec<bool>,
     pub tunnel: Vec<bool>,
+}
+
+pub struct RoadIndex<T: RTreeObject + PointDistance + Clone> {
+    pub index: RTree<GeomWithData<T, u64>>,
+}
+
+impl<T: RTreeObject + PointDistance + Clone> RoadIndex<T> {
+    pub fn new() -> RoadIndex<T> {
+        Self {
+            index: RTree::new(),
+        }
+    }
+
+    pub fn from(ids: &[u64], roads: &[T]) -> RoadIndex<T> {
+        let geomdata: Vec<GeomWithData<T, u64>> = roads
+            .iter()
+            .zip(ids.iter())
+            .map(|(road, id)| GeomWithData::<T, u64>::new(road.clone(), *id))
+            .collect();
+
+        RoadIndex::<T> {
+            index: RTree::<GeomWithData<T, u64>>::bulk_load(geomdata),
+        }
+    }
+
+    pub fn insert(&mut self, id: u64, road: T) {
+        let geomdata: GeomWithData<T, u64> = GeomWithData::new(road, id);
+        self.index.insert(geomdata);
+    }
+
+    pub fn empty(&mut self) {
+        self.index = RTree::<GeomWithData<T, u64>>::new();
+    }
+
+    pub fn remove(&mut self, _id: u64) {
+        todo!()
+    }
 }
 
 pub struct NameKey(pub Id);
