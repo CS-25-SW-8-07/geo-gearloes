94fb9c7603290c21bb4cbde169c634eb80b75cd6
Anders Rask Sørensen <61500734+Uewotm90@users.noreply.github.com>
2025-03-11 14:00:07 +0100

    4-box-query-but-specify-which-roads-should-not-be-returned-by-id (#14)
    * extracted sql query into variable for debugabilty + test case for new function
    
    * removed deprecated and unused function
    
    * removed deprecated box query and its test
    
    * removed deprecated function
    
    * removed unused type
    
    * temporary allows, just in case it is going to be used
    
    * added test case using malform bounding box
    
    * added very helpful comment
    
    * rewrote box_query to use box_query_without
    
    * removed unused type
    
    * format
    
    * bind -> create_pool
    
    * applied clippy suggestion (avoid using format in in loop when write!() is more appropiate
    
    * removed old commented code
    
    * fixed the fix
    
    * added new lint
    
    * applied new lint
    
    * new name for box_query_without
    
    * added dependency
    
    * added error type + error module
    
    * added error type where possible
    
    * removed unused use'es
    
    * handle edge case if given an empty multilinestring (empty linestrings are still allowed)
    
    * now handles empty multilinestrings instead of panicking, empty linestrings are still allowed

--- atlas/src/lib.rs
+++ atlas/src/lib.rs
@@ -1,4 +1,7 @@
+mod error;
+
 use derive_more::From;
+use error::DbError;
 use geo_traits::to_geo::ToGeoGeometry;
 use geo_types::{Geometry, LineString};
 use rusty_roads::*;
@@ -7,25 +10,10 @@ use sqlx::{
     postgres::{self, PgPoolOptions},
     FromRow, Pool, Postgres, Row,
 };
+use std::fmt::Write;
 use wkb::reader::read_wkb;
 
 type Bbox<T> = ((T, T), (T, T));
-type _DbRoad = (
-    i32,
-    String,
-    i16,
-    String,
-    Option<String>,
-    Option<String>,
-    String,
-    i16,
-    f64,
-    String,
-    String,
-    Vec<u8>,
-);
-
-type DbRoad = (i32, Vec<u8>, i64, i16, String, i16, i16, bool, bool); //TODO: osm id is actually u64, other signed/unsigned funny business
 
 #[derive(sqlx::Type, From)]
 #[sqlx(transparent, no_pg_array)]
@@ -33,37 +21,45 @@ struct MyRoad(Road);
 
 #[derive(sqlx::FromRow, From)]
 #[sqlx(transparent)]
+#[allow(dead_code)]
 struct MyNameRow(NameRow);
 
 #[derive(sqlx::FromRow, From)]
 #[sqlx(transparent)]
+#[allow(dead_code)]
 struct MyRefManyKey(RefManyKey);
 
 #[derive(sqlx::FromRow, From)]
 #[sqlx(transparent)]
+#[allow(dead_code)]
 struct MyFeatureClassRow(FeatureClassRow);
 
 // hopefully other tables are automatically derivable
 impl FromRow<'_, postgres::PgRow> for MyRoad {
     fn from_row(row: &'_ postgres::PgRow) -> Result<Self, sqlx::Error> {
+        let id = row.try_get::<i32, _>("id")? as rusty_roads::Id;
         let ls = wkb_to_linestring(&row.try_get::<Vec<u8>, _>("geom")?).ok_or(
             sqlx::Error::ColumnDecode {
                 index: "geom".into(),
-                source: Box::new(sqlx::Error::ColumnNotFound("geom".into())),
+                source: Box::new(DbError::Linestring(id)),
             },
         )?;
         let direc = |c: &str| match c {
-            "B" => Some(Direction::Bidirectional),
-            "T" => Some(Direction::Backward),
-            "F" => Some(Direction::Forward),
-            _ => None,
+            "B" => Ok(Direction::Bidirectional),
+            "T" => Ok(Direction::Backward),
+            "F" => Ok(Direction::Forward),
+            e => Err(sqlx::Error::ColumnDecode {
+                index: "oneway".into(),
+                source: Box::new(DbError::DirectionDecode(e.into())),
+            }),
         };
+
         let road = Road {
-            id: row.try_get::<i32, _>("id")? as rusty_roads::Id,
+            id,
             geom: ls,
             osm_id: row.try_get::<i64, _>("osm_id")? as u64,
             code: row.try_get::<i16, _>("code")? as u16,
-            direction: direc(&row.try_get::<String, _>("oneway")?).expect("msg"),
+            direction: direc(&row.try_get::<String, _>("oneway")?)?,
             maxspeed: row.try_get::<i16, _>("maxspeed")? as u16,
             layer: row.try_get::<i16, _>("layer")?,
             bridge: row.try_get::<bool, _>("bridge")?,
@@ -73,89 +69,79 @@ impl FromRow<'_, postgres::PgRow> for MyRoad {
     }
 }
 
-pub async fn bind(conn: &str, max_conn: Option<u32>) -> Result<Pool<Postgres>, sqlx::Error> {
-    //TODO: denne funktion kunne evt. også stå for at sætte prepared statements op
+/// Lazily creates a connection [`Pool`] with a given maximum connections (default 1).
+///
+/// # Errors
+///
+/// This function will return an error if the connection issues (e.g. malformed connection string, or if the database is down).
+pub async fn create_pool(conn: &str, max_conn: Option<u32>) -> Result<Pool<Postgres>, sqlx::Error> {
     PgPoolOptions::new()
         .max_connections(max_conn.unwrap_or(1))
         .connect_lazy(conn)
 }
 
-#[deprecated = "uses wrong table, use `box_query` instead"]
-pub async fn box_query_as(
-    mut conn: PoolConnection<Postgres>,
-    bbox: Bbox<f64>,
-) -> Result<Vec<rusty_roads::Road>, sqlx::Error> {
-    let (minx, miny, maxx, maxy) = (bbox.0 .0, bbox.0 .1, bbox.1 .0, bbox.1 .1);
-    let res: Vec<_DbRoad> = sqlx::query_as("with box as (select st_envelope( st_setsrid(st_collect(st_makepoint($1,$2),st_makepoint($3,$4)),4326) ) as bbox)
-select gid, osm_id, code, fclass, name, ref, oneway, maxspeed, layer, bridge, tunnel, st_asbinary(geom,'NDR') from public.gis_osm_roads_free_1
-join box on st_intersects(geom,bbox)").bind(minx).bind(miny).bind(maxx).bind(maxy).fetch_all(&mut *conn).await?; //TODO: query should be LIMIT'ed, maybe it should be a parameter
-    let res = res.into_iter().filter_map(to_road).collect::<Vec<_>>(); //TODO: should maybe report on any error in linestring construction
-    Ok(res)
-}
-
+/// Retrives [`Road`]s that intersect with a given bounding box, up to a limit, if given.
+///
+/// # Errors
+///
+/// This function will return an error if there are connection issues with the database.
 pub async fn box_query(
-    mut conn: PoolConnection<Postgres>,
+    conn: PoolConnection<Postgres>,
     bbox: Bbox<f64>,
     limit: Option<u32>,
-) -> Result<Vec<rusty_roads::Road>, sqlx::Error> {
-    let (minx, miny, maxx, maxy) = (bbox.0 .0, bbox.0 .1, bbox.1 .0, bbox.1 .1);
-    let limit = limit.map(|x| format!("limit {x}")).unwrap_or("".into()); //i could not get sql's LIMIT ALL to work, so this is a workaround
+) -> Result<Vec<rusty_roads::Road>, DbError> {
+    box_query_exclude_by_id(conn, bbox, &[], limit).await
+}
 
-    let res: Vec<MyRoad> = sqlx::query_as(&format!("with box as (select st_envelope( st_setsrid(st_collect(st_makepoint($1,$2),st_makepoint($3,$4)),4326) ) as bbox)
-select id, st_asbinary(st_geometryn(geom,1),'NDR') as geom, osm_id, code, oneway, maxspeed, layer, bridge, tunnel from roads
-join box on st_intersects(geom,bbox)
-{limit};")).bind(minx).bind(miny).bind(maxx).bind(maxy).fetch_all(&mut *conn).await?; // multilinestring gets converted to just linestring
+/// Like [`box_query`], but allows excluding certain roads by id.
+///
+/// # Errors
+///
+/// This function will return an error if there are connection issues with the database.
+pub async fn box_query_exclude_by_id(
+    mut conn: PoolConnection<Postgres>,
+    bbox: Bbox<f64>,
+    without: &[usize],
+    limit: Option<u32>,
+) -> Result<Vec<rusty_roads::Road>, DbError> {
+    let (minx, miny, maxx, maxy) = (bbox.0 .0, bbox.0 .1, bbox.1 .0, bbox.1 .1);
+    let limit = limit.map_or("".into(), |x| format!("limit {x}"));
+
+    let where_clause = match without.len() {
+        0 => "".into(),
+        _ => {
+            let not_in = without.iter().fold(String::new(), |mut acc, id| {
+                let _ = write!(acc, "{id},");
+                acc
+            });
+            let not_in = &not_in[0..not_in.len() - 1];
+            format!("where id not in ({not_in})")
+        }
+    };
+
+    let sql = format!("with box as (select st_envelope( st_setsrid(st_collect(st_makepoint($1,$2),st_makepoint($3,$4)),4326) ) as bbox)
+    select id, st_asbinary(st_geometryn(geom,1),'NDR') as geom, osm_id, code, oneway, maxspeed, layer, bridge, tunnel from roads
+    join box on st_intersects(geom,bbox)  {where_clause} 
+    {limit};");
+    let res: Vec<MyRoad> = sqlx::query_as(&sql)
+        .bind(minx)
+        .bind(miny)
+        .bind(maxx)
+        .bind(maxy)
+        .fetch_all(&mut *conn)
+        .await
+        .map_err(DbError::Sqlx)?;
+
     Ok(res.into_iter().map(|x| x.0).collect::<Vec<_>>())
 }
 
 fn wkb_to_linestring(bytea: &[u8]) -> Option<LineString<f64>> {
     let a = read_wkb(bytea).ok()?.try_to_geometry()?;
     match a {
-        Geometry::LineString(geom) => Some(geom),
-        Geometry::MultiLineString(geoms) => Some(geoms.0[0].clone()),
+        Geometry::LineString(geom) => Some(geom), // empty linestringes are still allowed
+        Geometry::MultiLineString(geoms) => Some(geoms.0.get(0)?.clone()), // to avoid this, in sql, write st_geomtryn(geom,1)
         _ => None,
     }
-}
-
-#[deprecated]
-fn into_road(road: DbRoad) -> Option<Road> {
-    let ls = wkb_to_linestring(&road.1)?;
-    let direc = |c: &str| match c {
-        "B" => Some(Direction::Bidirectional),
-        "T" => Some(Direction::Backward),
-        "F" => Some(Direction::Forward),
-        _ => None,
-    };
-
-    // all data from the danish road dataset are within casting bounds
-    let res = Road {
-        id: road.0 as rusty_roads::Id,
-        geom: ls,
-        osm_id: road.2 as u64,
-        code: road.3 as u16,
-        direction: direc(&road.4)?, //FIXME
-        maxspeed: road.5 as u16,
-        layer: road.6,
-        bridge: road.7,
-        tunnel: road.8,
-    };
-    Some(res)
-}
-
-#[deprecated]
-fn to_road(row: _DbRoad) -> Option<Road> {
-    let ls = wkb_to_linestring(&row.11)?;
-    Some(Road {
-        id: row.0 as rusty_roads::Id,
-        geom: ls,
-        osm_id: row.1.parse::<u64>().ok()?,
-        code: row.2 as u16,                  //FIXME
-        direction: Direction::Bidirectional, //FIXME
-        maxspeed: row.7 as u16,              //FIXME
-        layer: row.8 as i16,                 //TODO: probably an error in schema
-        bridge: true,                        //FIXME
-        tunnel: true,                        //FIXME
-    })
 }
 
 #[cfg(test)]
@@ -186,45 +172,83 @@ mod tests {
         )
     });
     static POOL: LazyLock<Pool<Postgres>> = LazyLock::new(|| {
-        async_std::task::block_on(async { bind(&CONN, Some(CONNCOUNT)).await.expect("msg") })
+        async_std::task::block_on(async {
+            create_pool(&*CONN, Some(CONNCOUNT)).await.expect("msg")
+        })
     });
 
+    static BBOX_CASSIOPEIA: LazyLock<Bbox<f64>> = LazyLock::new(|| {
+        (
+            (9.989492935608991, 57.009828137476511),
+            (9.995526228694693, 57.013236271456691),
+        )
+    });
+    const BBOX_CASSIOPEIA_COUNT: usize = 79;
     #[async_std::test]
     async fn it_connects() {
-        let pool = bind(&CONN, Some(1)).await;
+        let pool = create_pool(&*CONN, Some(1)).await;
         assert!(matches!(pool, Ok(x) if x.options().get_max_connections() >= 1))
     }
 
-    #[async_std::test]
-    #[ignore = "function uses old table structure"]
-    async fn sorry_to_box_in_old() {
-        let pool = bind(&CONN, Some(1))
-            .await
-            .expect("Failed to connect to database, perhaps it is offline");
-        let res = box_query_as(
-            pool.acquire().await.expect("failed to acquire connection"),
-            (
-                (9.989492935608991, 57.009_828_137_476_51),
-                (9.995526228694693, 57.013_236_271_456_69),
-            ),
-        )
-        .await
-        .expect("error in box query");
-        assert_eq!(res.len(), 79)
+    mod box_query {
+        use std::f64::NAN;
+
+        use super::*;
+
+        #[async_std::test]
+        async fn sorry_to_box_in() {
+            let conn = (*POOL)
+                .acquire()
+                .await
+                .expect("failed to establish database connection, perhaps it is closed");
+            let res = box_query(conn, *BBOX_CASSIOPEIA, None).await;
+            assert!(
+                matches!(&res, Ok(x) if x.len()==BBOX_CASSIOPEIA_COUNT),
+                "x.len()=={}",
+                res.map(|x| x.len()).unwrap_or(0)
+            )
+        }
+        #[async_std::test]
+        async fn sorry_to_box_in_illegal_box() {
+            let bbox = ((NAN, NAN), (NAN, NAN));
+            const NAN_BOX: usize = 0;
+            let conn = (*POOL)
+                .acquire()
+                .await
+                .expect("failed to establish database connection, perhaps it is closed");
+            let res = box_query(conn, bbox, None)
+                .await
+                .expect("failed to execute query");
+            assert_eq!(NAN_BOX, res.len())
+        }
     }
 
-    #[async_std::test]
-    async fn sorry_to_box_in() {
-        let bbox_cassiopeia = (
-            (9.989492935608991, 57.009_828_137_476_51),
-            (9.995526228694693, 57.013_236_271_456_69),
-        );
-        let conn = (*POOL).acquire().await.expect("msg");
-        let res = box_query(conn, bbox_cassiopeia, None).await;
-        assert!(
-            matches!(&res, Ok(x) if x.len()==79),
-            "x.len()=={}",
-            res.map(|x| x.len()).unwrap_or(0)
-        )
+    mod box_query_without {
+        use super::*;
+
+        #[async_std::test]
+        async fn box_without() {
+            let without = [592125, 592124, 661737];
+            let conn = (*POOL)
+                .acquire()
+                .await
+                .expect("failed to establish database connection, perhaps it is closed");
+            let res = box_query_exclude_by_id(conn, *BBOX_CASSIOPEIA, &without, None)
+                .await
+                .expect("failed to execute query");
+            assert_eq!(BBOX_CASSIOPEIA_COUNT - without.len(), res.len())
+        }
+
+        #[async_std::test]
+        async fn box_without_but_empty() {
+            let conn = (*POOL)
+                .acquire()
+                .await
+                .expect("failed to establish database connection, perhaps it is closed");
+            let res = box_query_exclude_by_id(conn, *BBOX_CASSIOPEIA, &[], None)
+                .await
+                .expect("failed to execute query");
+            assert_eq!(BBOX_CASSIOPEIA_COUNT, res.len())
+        }
     }
 }
