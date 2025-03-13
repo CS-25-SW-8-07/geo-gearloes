e5c4a36ed30f7364b829f1f1320170d56d83a8c1
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-03-13 12:45:52 +0100

    initial

--- atlas/src/lib.rs
+++ atlas/src/lib.rs
@@ -1,4 +1,5 @@
 mod error;
+mod porto;
 
 use derive_more::From;
 use error::DbError;
---
+++ atlas/src/porto.rs
@@ -0,0 +1,8 @@
+
+fn map_match_porto() {
+    const CHUNK_SIZE: i32 = 10000;
+    const _: () = assert!(CHUNK_SIZE>0);
+
+
+    
+}
