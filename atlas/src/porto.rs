144945ca3c2d1bd6bed284d5c4ed65ec40e0b119
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2025-03-14 10:01:25 +0100

    wip

--- atlas/src/porto.rs
+++ atlas/src/porto.rs
@@ -1,24 +1,57 @@
+use std::collections::HashMap;
 use std::fmt::Write;
 use std::{borrow::Cow, collections::HashSet};
 
 use geo_types::{Line, LineString};
+use sqlx::Pool;
 use sqlx::{pool::PoolConnection, Postgres};
 
 use crate::wkb_to_linestring;
 
-pub async fn map_match_porto(mut conn: PoolConnection<Postgres>) -> Result<Vec<i32>, sqlx::Error> {
-    const CHUNK_SIZE: i32 = 10000;
+type Trajectory = (i32,LineString);
+
+pub async fn map_match_porto<T,F>(pool: Pool<Postgres>, f: F) -> Result<T, sqlx::Error>
+where F: Fn(&[Trajectory]) -> T {
+    const CHUNK_SIZE: i32 = 100000;
     const _: () = assert!(CHUNK_SIZE > 0);
     let ids: Vec<(i32,(/*sqlx be trolling */))> = sqlx::query_as("select id from taxadata;")
-        .fetch_all(&mut *conn)
+        .fetch_all(&pool)
         .await?;
 
-    let ko = Cow::from(ids.into_iter().map(|e| e.0).collect::<Vec<_>>().as_slice());
+    let ko = ids.into_iter().map(|e| e.0);
     // let ids: HashSet<i32> = HashSet::from_iter(ids.into_iter().map(|e| e.0));
-    let (ids, trajs): (Vec<i32>,Vec<LineString>) = get_trajectories(conn, CHUNK_SIZE, None).await?.into_iter().unzip();
-    
+    let mut received = HashMap::<i32, LineString>::with_capacity(ko.len());
+    let all_ids: HashSet<i32> = HashSet::from_iter(ko);
+    let mut keys: HashSet<i32> = HashSet::with_capacity(all_ids.len());
+    // let mut ids_copy: Vec<i32> = Vec::with_capacity(CHUNK_SIZE as usize);
+    while all_ids != keys {
+        let (ids, trajs): (Vec<i32>, Vec<LineString>) =
+            get_trajectories(pool.acquire().await?, CHUNK_SIZE, None)
+                .await?
+                .into_iter()
+                .unzip();
+        // ids_copy = ids.clone();
+        // let ress = get_trajectories(pool.acquire().await?, CHUNK_SIZE, None)
+        //     .await?;
+        keys = keys
+            .union(&HashSet::from_iter(ids.iter().copied()))
+            .copied()
+            .collect();
+        received.extend(ids.into_iter().zip(trajs.into_iter()));
 
-    
+
+    }
+
+    todo!()
+}
+
+async fn insert_matched_trajectories(trajs: &[Trajectory]) -> Result<(),sqlx::Error> {
+
+    todo!()
+}
+
+fn map_match(trajs: &[Trajectory]) -> Vec<Trajectory> {
+
     todo!()
 }
 
@@ -39,7 +72,9 @@ async fn get_trajectories(
             format!("where id not in ({not_in})")
         }
     };
-    let sql = format!("select id, st_asbinary(trajectory::geometry,'NDR') {where_clause} limit {chunk_size}");
+    let sql = format!(
+        "select id, st_asbinary(trajectory::geometry,'NDR') {where_clause} limit {chunk_size}"
+    );
 
     let trajectories: Vec<(i32, Vec<u8>)> = sqlx::query_as(&sql).fetch_all(&mut *conn).await?;
     let trajectories: Vec<(i32, LineString)> = trajectories
