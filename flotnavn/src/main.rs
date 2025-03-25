6f1ae8db814504dbb45f5f6af18c7c25efadf3b7
n9cklas1997 <165781290+n9cklas1997@users.noreply.github.com>
2025-03-25 07:30:37 +0100

    help

--- flotnavn/src/main.rs
+++ flotnavn/src/main.rs
@@ -4,11 +4,43 @@ use std::env;
 use atlas::{bind, box_query};
 
 
-
-/* #[get("/boundingbox")]
+#[get("/boundingbox")]
 async fn get_all_records(pool: web::Data<PgPool>) -> impl Responder {
-atlas::box_query(pool, ((1.0, 1.0),(1.0, 1.0)), None);
-} */
+    let bbox = ((8.4741452, 55.462423), (8.4743079, 55.4627272));
+    let conn = pool.acquire().await.unwrap();
+
+    match atlas::box_query(conn, bbox, None).await {
+        Ok(roads) => {
+            let result = roads.into_iter()
+                .map(|road| {
+                    let direction_str = match road.direction {
+                        rusty_roads::Direction::Forward => "Forward",
+                        rusty_roads::Direction::Backward => "Backward",
+                        rusty_roads::Direction::Bidirectional => "Bidirectional",
+                    };
+
+                    format!(
+                        "Road ID: {}\nOSM ID: {}\nCode: {}\nMaxSpeed: {}\nDirection: {}\nLayer: {}\nBridge: {}\nTunnel: {}\n\n", 
+                        road.id, 
+                        road.osm_id, 
+                        road.code, 
+                        road.maxspeed, 
+                        direction_str,
+                        road.layer,
+                        road.bridge,
+                        road.tunnel,
+                    )
+                })
+                .collect::<String>();
+
+            HttpResponse::Ok().content_type("text/plain").body(result)
+        },
+        Err(e) => HttpResponse::InternalServerError().body(format!("Error: {}", e)),
+    }
+}
+
+
+
 
 #[get("/test")]
 async fn testing123(pool: web::Data<PgPool>) -> impl Responder {
@@ -22,7 +54,7 @@ async fn testing123(pool: web::Data<PgPool>) -> impl Responder {
             HttpResponse::Ok().json(roadname)
         }
         Err(e) => {
-            eprintln!("Database error: {:?}", e); // Print error message in console
+            eprintln!("Database error: {:?}", e);
             HttpResponse::InternalServerError().body(format!("Database error: {:?}", e))
         }
     }
@@ -58,6 +90,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(web::Data::new(pool.clone())) // Share the pool across all routes
             .service(testing123)
+            .service(get_all_records)
     })
     .bind(("127.0.0.1", 8080))? // Bind to localhost:8080
     .run()
